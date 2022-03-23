ec9ebb3f0b7889907aff9e990e8659e00187598c
Kim yonggwan <sssw0101@naver.com>
2022-03-23 22:13:25 +0900

    양방향 연관관계와 연관관계의 주인 1-기본

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -24,13 +25,18 @@ public class JpaMain {
             member.setTeam(team);   //단방향 연관관계 설정, 참조 저장
             em.persist(member);
 
+            em.flush();
+            em.clear();
+
             Member findMember = em.find(Member.class, member.getId());
             Team findTeam = findMember.getTeam();
             System.out.println("findTeam = " + findTeam.getName());
 
-            //팀 수정
-            Team newTeam = em.find(Team.class, 100L);
-            findMember.setTeam(newTeam);
+            List<Member> members = findMember.getTeam().getMembers();
+            
+            for(Member m : members){
+                System.out.println("m.getUsername() = " + m.getUsername());
+            }
 
             tx.commit();
         }catch (Exception e){
