c47bc8babdb805c40ba8473eba4d7643dc81cbe0
Kim yonggwan <sssw0101@naver.com>
2022-04-05 17:58:28 +0900

    상속관계 매핑 - 구현 클래스마다 테이블 전략

--- src/main/java/hellojpa/Item.java
+++ src/main/java/hellojpa/Item.java
@@ -3,9 +3,9 @@ package hellojpa;
 import javax.persistence.*;
 
 @Entity
-@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
-@DiscriminatorColumn(name = "ITEM_TYPE")
-public class Item {
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+@DiscriminatorColumn
+public abstract class Item {
 
     @Id @GeneratedValue
     @Column(name = "ITEM_ID")
