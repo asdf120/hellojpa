7f200334602e81860dfd19075c890e5c46d7b10c
Kim yonggwan <sssw0101@naver.com>
2022-04-06 14:52:51 +0900

    Mapped Superclass - 매핑 정보 상속

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.time.LocalDateTime;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -15,18 +16,15 @@ public class JpaMain {
 
         try {
             // 저장
-            Movie movie = new Movie();
-            movie.setDirector("감독님");
-            movie.setActor("배우");
-            movie.setName("영화");
-            movie.setPrice(10000);
+            Member member = new Member();
+            member.setUsername("user");
+            member.setCreatedBy("kim");
+            member.setCreatedDate(LocalDateTime.now());
+            em.persist(member);
 
-            em.persist(movie);
             em.flush();
             em.clear();
 
-            Movie findMovie = em.find(Movie.class, movie.getId());
-            System.out.println("findMovie = " + findMovie);
 
             tx.commit();
         }catch (Exception e){
