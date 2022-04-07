a7eb1c2fa29fd5cd15358c0b693419db1cbda9d3
Kim yonggwan <sssw0101@naver.com>
2022-04-07 11:40:04 +0900

    실전 예제 4 - 상속관계 매핑

--- src/main/java/jpabook/jpashop/domain/Item.java
+++ src/main/java/jpabook/jpashop/domain/Item.java
@@ -5,7 +5,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-public class Item {
+@Inheritance(strategy = InheritanceType.JOINED)
+@DiscriminatorColumn(name = "DTYPE")
+public class Item extends BaseEntity{
 
     @Id @GeneratedValue
     @Column(name = "ITEM_ID")
