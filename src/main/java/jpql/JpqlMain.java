613c9f57c80ec6c8ec9915d20665d7e2881d9ff9
Kim yonggwan <sssw0101@naver.com>
2022-04-13 23:25:56 +0900

    조건식

--- src/main/java/jpql/JpqlMain.java
+++ src/main/java/jpql/JpqlMain.java
@@ -20,7 +20,7 @@ public class JpqlMain {
             em.persist(team);
 
             Member member = new Member();
-            member.setUsername("teamA");
+            member.setUsername("관리자");
             member.setAge(30);
             member.changeTeam(team);
             member.setType(MemberType.ADMIN);
@@ -49,17 +49,50 @@ public class JpqlMain {
 //            System.out.println("result = " + result.size());
 
             // 타입 표현
-            String query = "SELECT m.username, 'HELLO', TRUE FROM Member m" +
-                    " where m.type = :userType ";
-            List<Object[]> result = em.createQuery(query)
-                    .setParameter("userType", MemberType.ADMIN)
+//            String query = "SELECT m.username, 'HELLO', TRUE FROM Member m" +
+//                    " where m.type = :userType ";
+//            List<Object[]> result = em.createQuery(query)
+//                    .setParameter("userType", MemberType.ADMIN)
+//                    .getResultList();
+//
+//            for (Object[] objects : result) {
+//                System.out.println("objects[0] = " + objects[0]);
+//                System.out.println("objects[1] = " + objects[1]);
+//                System.out.println("objects[2] = " + objects[2]);
+//            }
+
+            // 기본 CASE식
+//            String query = "SELECT" +
+//                    " CASE WHEN m.age <= 10 THEN '학생요금'" +
+//                    "       WHEN m.age >= 60 THEN '경로요금'" +
+//                    "       ELSE '일반요금'" +
+//                    " END" +
+//                    " FROM Member m";
+//
+//            List<String> resultList = em.createQuery(query, String.class)
+//                    .getResultList();
+//            for (String s : resultList) {
+//                System.out.println("s = " + s);
+//            }
+
+            // 조건식 - coalesce
+//            String query = "SELECT COALESCE(m.username, '이름 없는 회원') FROM Member m ";
+//            List<String> resultList = em.createQuery(query, String.class)
+//                    .getResultList();
+//
+//            for (String s : resultList) {
+//                System.out.println("s = " + s);
+//            }
+
+            // 조건식 - NULLIF
+            String query = "SELECT NULLIF(m.username, '관리자') FROM Member m ";
+            List<String> resultList = em.createQuery(query, String.class)
                     .getResultList();
 
-            for (Object[] objects : result) {
-                System.out.println("objects[0] = " + objects[0]);
-                System.out.println("objects[1] = " + objects[1]);
-                System.out.println("objects[2] = " + objects[2]);
+            for (String s : resultList) {
+                System.out.println("s = " + s);
             }
+
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
