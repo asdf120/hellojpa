a461b90da6bc67b48e2b2bfbb969aa16f2f2aacf
Kim yonggwan <sssw0101@naver.com>
2022-04-13 15:15:47 +0900

    객체지향 쿼리 언어 - 기본문법 JPQL

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -5,6 +5,7 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 import java.time.LocalDateTime;
+import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -15,21 +16,10 @@ public class JpaMain {
         tx.begin();
 
         try {
-            // 저장
-            Team team = new Team();
-            team.setName("team1");
-            em.persist(team);
-
-            Member member1 = new Member();
-            member1.setUsername("member1");
-            member1.setTeam(team);
-            em.persist(member1);
-
-            em.flush();
-            em.clear();
-
-            Member findMember = em.find(Member.class, member1.getId());
-            System.out.println("findMember.getTeam().getClass() = " + findMember.getTeam().getClass());
+            List<Member> result = em.createQuery(
+                    "SELECT m FROM Member m WHERE m.username LIKE '%kim%'",
+                    Member.class
+            ).getResultList();
 
             tx.commit();
         }catch (Exception e){
