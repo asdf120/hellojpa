f17d42fb715823e034b1e7118dc3e8ea7e89da3c
Kim yonggwan <sssw0101@naver.com>
2022-04-13 17:56:41 +0900

    프로젝션(SELECT)

--- src/main/java/jpql/JpqlMain.java
+++ src/main/java/jpql/JpqlMain.java
@@ -1,6 +1,9 @@
 package jpql;
 
+import jpql.dto.MemberDTO;
+
 import javax.persistence.*;
+import java.util.List;
 
 public class JpqlMain {
     public static void main(String[] args) {
@@ -16,14 +19,39 @@ public class JpqlMain {
             member.setAge(30);
             em.persist(member);
 
-            Member result = em.createQuery("SELECT m FROM Member m where m.username = :username", Member.class)
-                            .setParameter("username", "member1")
-                                    .getSingleResult();
+            em.flush();
+            em.clear();
 
-            System.out.println("singleResult = " + result.getAge());
+            // 엔티티 프로젝션
+//            List<Member> result = em.createQuery("SELECT m FROM Member m", Member.class)
+//                            .getResultList();
+//            Member findMember = result.get(0);
+//            findMember.setAge(20);
 
-            TypedQuery<String> query2 = em.createQuery("SELECT m.username FROM Member m", String.class);
-            Query query3 = em.createQuery("SELECT m.username, m.age FROM Member m");
+            // 조인 엔티티 프로젝션
+//            List<Team> result = em.createQuery("SELECT t FROM Member m JOIN m.team t", Team.class)
+//                    .getResultList();
+            
+            // 임베디드 타입 프로젝션
+//            em.createQuery("SELECT o.address from Order o", Address.class)
+//                    .getResultList();
+
+            // 스칼라 프로젝션 - Object[] 타입으로 조회
+//            List resultList = em.createQuery("SELECT DISTINCT m.username, m.age from Member m")
+//                    .getResultList();
+//            Object o = resultList.get(0);
+//            Object[] result = (Object[]) o;
+//            System.out.println("username = " + result[0]);
+//            System.out.println("age = " + result[1]);
+
+            // 스칼라 프로젝션 - new 명령어로 조회
+            List<MemberDTO> resultList = em.createQuery("SELECT new jpql.dto.MemberDTO(m.username, m.age) FROM Member m", MemberDTO.class)
+                    .getResultList();
+
+            for (MemberDTO memberDTO : resultList) {
+                System.out.println("memberDTO username= " + memberDTO.getUsername());
+                System.out.println("memberDTO age= " + memberDTO.getAge());
+            }
 
             tx.commit();
         } catch (Exception e) {
