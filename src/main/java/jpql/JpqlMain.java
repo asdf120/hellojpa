478b202385e4a0d06c72533b7291979614897331
Kim yonggwan <sssw0101@naver.com>
2022-04-16 16:07:54 +0900

    페치 조인 - 기본

--- src/main/java/jpql/JpqlMain.java
+++ src/main/java/jpql/JpqlMain.java
@@ -16,34 +16,65 @@ public class JpqlMain {
         tx.begin();
 
         try {
-            Team team = new Team();
-            team.setName("teamA");
-            em.persist(team);
+            Team teamA = new Team();
+            teamA.setName("teamA");
+            em.persist(teamA);
+
+            Team teamB = new Team();
+            teamB.setName("teamB");
+            em.persist(teamB);
 
             Member member = new Member();
-            member.setUsername("관리자");
+            member.setUsername("회원1");
             member.setAge(30);
-            member.changeTeam(team);
+            member.changeTeam(teamA);
             member.setType(MemberType.ADMIN);
             em.persist(member);
+
+            Member member2 = new Member();
+            member2.setUsername("회원2");
+            member2.setTeam(teamA);
+            em.persist(member2);
+
+            Member member3 = new Member();
+            member3.setUsername("회원3");
+            member3.setTeam(teamB);
+            em.persist(member3);
 
             em.flush();
             em.clear();
 
+            //  엔티티 페치 조인
+//            String query = "SELECT m FROM Member m JOIN FETCH m.team";
+
+            // 컬렉션 페치 조인
+            String query = "SELECT DISTINCT t FROM Team t JOIN t.members";
+
+            List<Team> resultList = em.createQuery(query, Team.class)
+                    .getResultList();
+
+            for (Team team : resultList) {
+                System.out.println("team = " + team.getName() + " | members=" + team.getMembers().size());
+                for(Member m : team.getMembers()){
+                    System.out.println("m = " + m);
+                }
+            }
+
+
             // INNER 조인
-//            String query = "SELECT m FROM Member m JOIN m.team t";
+//            String query = "SELECT m FROM Member m JOIN m.teamA t";
 
             // OUTER 조인
-//            String query = "SELECT m FROM Member m LEFT JOIN m.team t";
+//            String query = "SELECT m FROM Member m LEFT JOIN m.teamA t";
 
             // 세타 조인
 //            String query = "SELECT m FROM Member m, Team t WHERE m.username = t.name";
 
             // 조인대상 필터링
-//            String query = "SELECT m FROM Member m LEFT JOIN m.team t ON t.name = 'teamA'";
+//            String query = "SELECT m FROM Member m LEFT JOIN m.teamA t ON t.name = 'teamA'";
             
             // 연관관계 없는 외부조인
-//            String query = "SELECT m FROM Member m LEFT JOIN m.team t ON m.username = t.name";
+//            String query = "SELECT m FROM Member m LEFT JOIN m.teamA t ON m.username = t.name";
 //            List<Member> result = em.createQuery(query, Member.class)
 //                    .getResultList();
 
@@ -102,22 +133,19 @@ public class JpqlMain {
 
 
             // 사용자 정의 함수 호출
-//            Member member2 = new Member();
-//            member2.setUsername("meber2");
-//            em.persist(member2);
+//
 //
 //            String query = "SELECT FUNCTION('group_concat', m.username) FROM Member m";
 //            List<String> resultList = em.createQuery(query, String.class)
 //                    .getResultList();
 
             // 경로 표현식
-//            String query = "SELECT m.team From Member m";
-            String query = "SELECT t.members.size From Team t";
+//            String query = "SELECT m.teamA From Member m";
+//            String query = "SELECT t.members.size From Team t";
 
-            Integer resultList = em.createQuery(query, Integer.class)
-                    .getSingleResult();
+//            Integer resultList = em.createQuery(query, Integer.class)
+//                    .getSingleResult();
 
-            System.out.println("resultList = " + resultList);
 //            for (Object s : resultList) {
 //                System.out.println("s = " + s);
 //            }
